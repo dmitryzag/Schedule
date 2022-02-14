0875d62102ba82d8639c15925f3a5bedb4b8192d
Dmitry <zagibalovdmitry@gmail.com>
2022-02-14 14:00:28 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    // s = checkTime(s);
+    document.getElementById('txt').innerHTML =  "ВРЕМЯ: " + checkTime(h) + ":" + m ;
+    setTimeout(startTime, 1000);
+  }
+  
+
+
+startTime();
